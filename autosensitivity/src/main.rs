b94bcd66e1cccd73dcabbf141d8af0ba816ed1c9
Olivier Wenger <olivier.wenger@liip.ch>
2023-08-23 18:53:41 +0200

    writing works

--- autosensitivity/src/main.rs
+++ autosensitivity/src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use std::process::Command;
+use home::home_dir;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::{path::PathBuf, process::Command};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None, arg_required_else_help = true)]
@@ -10,16 +14,17 @@ struct Args {
 
     /// Load config
     #[arg(short, long, action)]
-    load: bool
+    load: bool,
 }
 
 fn main() {
     let args = Args::parse();
 
+    let config_dir = load_config_directory();
 
-    if let Some(save) = args.save{
+    if let Some(save) = args.save {
         println!("Saving config {}", save);
-        save_config(save);
+        save_config(save, config_dir);
     }
 
     if args.load {
@@ -27,8 +32,19 @@ fn main() {
     }
 }
 
-fn save_config(input_name: String) {
+fn load_config_directory() -> PathBuf {
+    let home_dir = home_dir();
 
+    if let Some(mut config_dir) = home_dir {
+        config_dir.push(".config");
+        config_dir.push("autosensitivity");
+        let _ = fs::create_dir_all(config_dir.as_path());
+        return config_dir;
+    }
+    panic!("Couldn't load config directory")
+}
+
+fn save_config(input_name: String, config_dir: PathBuf) {
     let output = Command::new("xinput")
         .arg("list")
         .arg("--id-only")
@@ -36,7 +52,9 @@ fn save_config(input_name: String) {
         .output()
         .expect("failed to run xinput, is it installed ?");
     println!("output: {}!", String::from_utf8_lossy(&output.stdout));
-    
-    // let mut config_file = File::create(input_name)?;
-    // file.write_all(accel_speed)
+    let mut input_path = config_dir;
+    input_path.push(input_name);
+
+    let config_file = File::create(input_path);
+    let _ = config_file.expect("Should exist").write_all(b"accel_speed");
 }
