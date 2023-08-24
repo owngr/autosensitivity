55a02f88850840787deadb0a91263b7cf5cdcb6a
Olivier Wenger <olivier.wenger@liip.ch>
2023-08-24 10:57:14 +0200

    saving config works

--- autosensitivity/src/main.rs
+++ autosensitivity/src/main.rs
@@ -3,7 +3,7 @@ use home::home_dir;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
-use std::{path::PathBuf, process::Command};
+use std::{path::PathBuf, process::{Command, Stdio}};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about=None, arg_required_else_help = true)]
@@ -24,7 +24,7 @@ fn main() {
 
     if let Some(save) = args.save {
         println!("Saving config {}", save);
-        save_config(save, config_dir);
+        let _ = save_config(save, config_dir);
     }
 
     if args.load {
@@ -44,17 +44,51 @@ fn load_config_directory() -> PathBuf {
     panic!("Couldn't load config directory")
 }
 
-fn save_config(input_name: String, config_dir: PathBuf) {
-    let output = Command::new("xinput")
+fn save_config(input_name: String, config_dir: PathBuf) -> std::io::Result<()> {
+    let output_list = Command::new("xinput")
         .arg("list")
         .arg("--id-only")
         .arg(format!("pointer:{}", input_name))
         .output()
         .expect("failed to run xinput, is it installed ?");
-    println!("output: {}!", String::from_utf8_lossy(&output.stdout));
-    let mut input_path = config_dir;
-    input_path.push(input_name);
+    if !output_list.status.success() {
+        panic!("Xinput: {}", String::from_utf8_lossy(&output_list.stderr));
+    }
 
-    let config_file = File::create(input_path);
-    let _ = config_file.expect("Should exist").write_all(b"accel_speed");
+    let mut input_id = String::from_utf8_lossy(&output_list.stdout).to_string();
+    input_id.pop();
+
+    let list_props = Command::new("xinput")
+        .arg("list-props")
+        .arg(input_id)
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let grep_accel_speed = Command::new("grep")
+        .arg("libinput Accel Speed (")
+        .stdin(Stdio::from(list_props.stdout.unwrap()))
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let output_accel_speed = grep_accel_speed.wait_with_output().unwrap();
+
+    if !output_accel_speed.status.success() {
+        panic!("Xinput: {}", String::from_utf8_lossy(&output_accel_speed.stderr));
+    }
+
+
+    let binding = String::from_utf8_lossy(&output_accel_speed.stdout);
+    let accel_speed_option = binding.split("\t").last();
+    if let Some(accel_speed) = accel_speed_option {
+        let mut input_path = config_dir;
+        input_path.push(input_name);
+
+        let mut config_file = File::create(input_path)?;
+        write!(config_file, "libinput Accel Speed\t{}", accel_speed)
+
+    } else {
+        panic!("Couldn't save the config to a file")
+    }
 }
